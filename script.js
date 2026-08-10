666a5b5619cced8d79edc47b1c11d24969e8b47b
juuanevan <juliana.rubel.assis@escola.pr.gov.br>
2026-08-10 11:54:28 -0300

    criamos um botao

---
+++ script.js
@@ -0,0 +1,14 @@
+// botões
+const botões = document.querySelectorAll('button');
+
+// para cada botão podemos curtir
+botoes.forEach(botão => {
+    let curtiu = false;
+
+     botao.addEventListener("click", curtir)
+
+function cutir(){
+    let num = botao.querySelector('span');
+}
+   num.textContent++; 
+});
